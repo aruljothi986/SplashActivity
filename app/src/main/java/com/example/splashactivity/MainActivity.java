04983600f29a3e21bfe34b690d7898173699f8dd
Arul jothi <aruljothi07@gmail.com>
2024-02-28 21:15:54 -0800

    First commit

---
+++ app/src/main/java/com/example/splashactivity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.splashactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // ipo na empty activity create pantern paarunga mam
+    }
+}
